7187bce73069dfb1fbf32a7b06ed73466c96e1ec
santuele-cl <santuele.cl@gmail.com>
2024-03-07 20:53:18 +0800

    fix: diagnoses table layout

--- src/app/dashboard/patients/[patientId]/diagnoses/page.tsx
+++ src/app/dashboard/patients/[patientId]/diagnoses/page.tsx
@@ -18,47 +18,50 @@ const DiagnosisPage = async ({
 }: {
   params: { patientId: string };
 }) => {
-  const diagnoses = await getDiagnosesByPatientId(patientId);
+  const response = await getDiagnosesByPatientId(patientId);
+  const diagnoses = response.data;
   console.log(diagnoses);
   return (
     <TableContainer>
       <Table sx={{ minWidth: 650, overflow: "auto" }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="right">Condition</TableCell>
-            <TableCell align="right">Date Diagnosed</TableCell>
+            <TableCell align="left">Condition</TableCell>
+            <TableCell align="left">Date Diagnosed</TableCell>
+            <TableCell align="left">Diagnose by</TableCell>
             <TableCell align="right">Details</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {diagnoses &&
-            diagnoses?.data &&
-            diagnoses.data.map((diagnosis) => (
-              <TableRow
-                key={diagnosis.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {diagnosis.id}
-                </TableCell>
-                <TableCell align="right">{diagnosis.condition}</TableCell>
-                <TableCell align="right">{`${format(
-                  diagnosis.diagnosisDate,
-                  " MMMM d, yyyy"
-                )}`}</TableCell>
-                <TableCell align="right">
-                  <Button
-                    variant="contained"
-                    LinkComponent={Link}
-                    href={`/dashboard/patients/${patientId}/diagnoses/${diagnosis.id}`}
-                  >
-                    View
-                  </Button>
-                  {/* <Button variant="outlined">Select</Button> */}
-                </TableCell>
-              </TableRow>
-            ))}
+            diagnoses.length > 0 &&
+            diagnoses.map((diagnosis) => {
+              const physician = diagnosis.physician;
+              return (
+                <TableRow
+                  key={diagnosis.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell align="left">{diagnosis.condition}</TableCell>
+                  <TableCell align="left">{`${format(
+                    diagnosis.diagnosisDate,
+                    " MMMM d, yyyy"
+                  )}`}</TableCell>
+                  <TableCell align="left">{`${physician?.employeeRole?.roleName} ${physician?.fname} ${physician?.lname} `}</TableCell>
+
+                  <TableCell align="right">
+                    <Button
+                      variant="contained"
+                      LinkComponent={Link}
+                      href={`/dashboard/patients/${patientId}/diagnoses/${diagnosis.id}`}
+                    >
+                      View
+                    </Button>
+                    {/* <Button variant="outlined">Select</Button> */}
+                  </TableCell>
+                </TableRow>
+              );
+            })}
         </TableBody>
       </Table>
     </TableContainer>
