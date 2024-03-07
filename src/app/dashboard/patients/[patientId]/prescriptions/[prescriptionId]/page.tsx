478045e44bccc051744d41adb09223922a0e3c29
santuele-cl <santuele.cl@gmail.com>
2024-03-07 20:58:51 +0800

    chore: add todo

--- src/app/dashboard/patients/[patientId]/prescriptions/[prescriptionId]/page.tsx
+++ src/app/dashboard/patients/[patientId]/prescriptions/[prescriptionId]/page.tsx
@@ -1,6 +1,9 @@
-import { getPrescriptionByPrescriptionId, getPrescriptionsByPatientId } from "@/actions/patients/prescriptions";
+import {
+  getPrescriptionByPrescriptionId,
+  getPrescriptionsByPatientId,
+} from "@/actions/patients/prescriptions";
 import { Stack } from "@mui/system";
-import LocalPharmacyIcon from '@mui/icons-material/LocalPharmacy';
+import LocalPharmacyIcon from "@mui/icons-material/LocalPharmacy";
 import Prescriptions from "../../visit-history/[visitId]/_components/Prescriptions";
 import Prescription from "../../visit-history/[visitId]/_components/Prescription";
 
@@ -9,26 +12,27 @@ const PrescriptionPage = async ({
 }: {
   params: { prescriptionId: string };
 }) => {
-  const response = await getPrescriptionByPrescriptionId
-  (prescriptionId);
-  const prescription = response.data
+  const response = await getPrescriptionByPrescriptionId(prescriptionId);
+  const prescription = response.data;
+
+  // TODO: (Fix) Drug name should be in heading instead of patient ID
   return (
     <Stack
-    sx={{
-      flexDirection: "row",
-      p: 1,
-      border: "1px solid rgba(0,0,0,0.1)",
-      borderRadius: "4px",
-    }}
-    gap={2}
-  >
-    <Stack sx={{ p: 3 }}>
-      <LocalPharmacyIcon sx={{ fontSize: 40 }} />
-    </Stack>
-    <Stack sx={{ flexGrow: "1", p: 2, gap: 2 }}>
+      sx={{
+        flexDirection: "row",
+        p: 1,
+        border: "1px solid rgba(0,0,0,0.1)",
+        borderRadius: "4px",
+      }}
+      gap={2}
+    >
+      <Stack sx={{ p: 3 }}>
+        <LocalPharmacyIcon sx={{ fontSize: 40 }} />
+      </Stack>
+      <Stack sx={{ flexGrow: "1", p: 2, gap: 2 }}>
         <Prescription prescription={prescription!} key={prescription?.id} />
+      </Stack>
     </Stack>
-  </Stack>
-  )
+  );
 };
 export default PrescriptionPage;
