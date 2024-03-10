43acaa290444a30b6ae7ae4977a4d48bdebca87c
santuele-cl <santuele.cl@gmail.com>
2024-03-10 10:24:29 +0800

    style: add space

--- src/app/dashboard/patients/[patientId]/vaccinations/page.tsx
+++ src/app/dashboard/patients/[patientId]/vaccinations/page.tsx
@@ -11,7 +11,7 @@ const VaccinationsPage = async ({
     { id: "id", label: "ID" },
     { id: "vaccineName", label: "Vaccine name" },
     { id: "dosage", label: "Dosage" },
-    { id: "nextDueDate", label: "Next appointment", type: "date" },
+    { id: "nextDueDate", label: "Next appointment ", type: "date" },
   ];
   return (
     <div>
