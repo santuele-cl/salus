1f7d8e6848108cc3ae193154a4607a2e23b65b42
santuele-cl <santuele.cl@gmail.com>
2024-03-10 20:38:15 +0800

    style: remove unneccesary files

--- src/app/dashboard/appointments/pagebak.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { getMonth } from "@/app/_ui/dashboard/_utils/days";
-
-const AppointmentPage = () => {
-  const months = getMonth();
-  console.table(months);
-  return <div>{JSON.stringify(months.length)}</div>;
-};
-export default AppointmentPage;
